09865a90594862061dd7bee42b5f99905c3cabf1
lugark <black_ice@web.de>
2021-04-20 10:06:56 +0200

    change Unittest

--- tests/Jira/Board/ConfigurationTest.php
+++ tests/Jira/Board/ConfigurationTest.php
@@ -16,6 +16,12 @@ class ConfigurationTest extends TestCase
         $this->jsonMapper = new \JsonMapper();
     }
 
+    /**
+     * @covers \App\Jira\Board\Configuration
+     * @covers \App\Jira\Board\Configuration\ColumnConfig
+     * @covers \App\Jira\Board\Configuration\ColumnConfig\Column
+     * @covers \App\Jira\Board\Configuration\ColumnConfig\MappingStatus
+     */
     public function testConfigMapping()
     {
         $json = file_get_contents(realpath(__DIR__ . '/fixtures/responseConfig.json'));
@@ -25,9 +31,21 @@ class ConfigurationTest extends TestCase
             new Configuration()
         );
 
-        $this->assertEquals('scrum', $configuration->type);
-        $this->assertEquals(5, count($configuration->columnConfig->columns));
-        $this->assertEquals('To Do', $configuration->columnConfig->columns[0]->name);
+        $configSerializable = $configuration->jsonSerialize();
+        $this->assertEquals('scrum', $configSerializable['type']);
+        $this->assertInstanceOf(Configuration\ColumnConfig::class, $configSerializable['columnConfig']);
+
+        $columnConfigSerializable = $configSerializable['columnConfig']->jsonSerialize();
+        $this->assertEquals(5, count($columnConfigSerializable['columns']));
+        $this->assertInstanceOf(Configuration\ColumnConfig\Column::class, $columnConfigSerializable['columns'][0]);
+
+        $columnSerializable = $columnConfigSerializable['columns'][0]->jsonSerialize();
+        $this->assertEquals('To Do', $columnSerializable['name']);
+        $this->assertEquals(3, count($columnSerializable['statuses']));
+        $this->assertInstanceOf(Configuration\ColumnConfig\MappingStatus::class, $columnSerializable['statuses'][0]);
+
+        $mappingSerializable = $columnSerializable['statuses'][0]->jsonSerialize();
+        $this->assertEquals($mappingSerializable['id'], 1122);
     }
 
 }
